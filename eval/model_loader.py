2928fe671c479bb7f7918a84e9136d4520dc3955
Tat Trong Vu <66179376+tattrongvu@users.noreply.github.com>
2023-11-15 10:07:59 +0100

    small fix

--- eval/model_loader.py
+++ eval/model_loader.py
@@ -179,13 +179,11 @@ def apply_patches(model, args):
             if "LlamaForCausalLM" in model.config.architectures:
                 patch_llama_for_linear_scaled_rotary_embeddings(
                     model, scale=args.linear)
-            '''
-            elif "FalconForCausalLM" in model.config.architectures:
-                patch_falcon_for_linear_scaled_rotary_embeddings(model, scale=args.linear)
-            '''
             else:
                 raise RuntimeError(
                     f"Unsupported architecture {model.config.architectures} for linear")
+            #elif "FalconForCausalLM" in model.config.architectures:
+            #    patch_falcon_for_linear_scaled_rotary_embeddings(model, scale=args.linear)
         elif args.part_ntk:
             if "LlamaForCausalLM" in model.config.architectures:
                 patch_llama_for_part_ntk_scaled_rotary_embeddings(
